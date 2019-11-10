e8bac1730d0fdb62d7feae2340f8c802ff820af5
Krishnan Ganesan <kganesan@salesforce.com>
2019-11-10 03:38:06 -0800

    Update to master

--- src/main/java/com/salesforce/placeorder/util/Constants.java
+++ src/main/java/com/salesforce/placeorder/util/Constants.java
@@ -1,30 +1,30 @@
 package com.salesforce.placeorder.util;
 
-import com.salesforce.placeorder.util.PropertiesReader.PROPERTY_TYPE;
+
 
 public class Constants {
-	private static PropertiesReader _holder = new PropertiesReader();
-	public static final String ADMIN_USERNAME = _holder.getProperty(PROPERTY_TYPE.BUILD, "admin.username");
-	public static final String ADMIN_PASSWORD = _holder.getProperty(PROPERTY_TYPE.BUILD, "admin.password");
 
-	public final static String FORCE_API_USERNAME = _holder.getProperty(PROPERTY_TYPE.BUILD, "forceapi.username");
-	public final static String FORCE_API_PASSWORD = _holder.getProperty(PROPERTY_TYPE.BUILD, "forceapi.password");
-	public final static String FORCE_API_ENDPOINT_PARTNER = _holder.getProperty(PROPERTY_TYPE.BUILD, "forceapi.partner.endpoint");
-	public final static String FORCE_API_ENDPOINT_METADATA = _holder.getProperty(PROPERTY_TYPE.BUILD, "forceapi.metadata.endpoint");
-	public final static String FORCE_API_ENDPOINT_APEX = _holder.getProperty(PROPERTY_TYPE.BUILD, "forceapi.apex.endpoint");
-	public final static boolean API_TRACE_ENABLED = (_holder.getProperty(PROPERTY_TYPE.BUILD, "forceapi.traceenabled") == "true") ? true : false;
-	public final static String FORCE_API_CONNECTION_TIMEOUT = _holder.getProperty(PROPERTY_TYPE.BUILD, "forceapi.connection.timeout");
-	public final static String FORCE_API_READ_TIMEOUT = _holder.getProperty(PROPERTY_TYPE.BUILD, "forceapi.read.timeout");
+	public static final String ADMIN_USERNAME = "p2cautomationuser@salesforce.com.finupgqa1";
+	public static final String ADMIN_PASSWORD = "Store123";
+
+	public final static String FORCE_API_USERNAME = "p2cautomationuser@salesforce.com.finupgqa1";
+	public final static String FORCE_API_PASSWORD = "Store123";
+	public final static String FORCE_API_ENDPOINT_PARTNER = "https://org62--finupgqa1.cs34.my.salesforce.com/services/Soap/u/47.0";
+	public final static String FORCE_API_ENDPOINT_METADATA = "https://org62--finupgqa1.cs34.my.salesforce.com/services/Soap/m/47.0";
+	public final static String FORCE_API_ENDPOINT_APEX = "https://org62--finupgqa1.cs34.my.salesforce.com/services/Soap/s/47.0";
+	public final static boolean API_TRACE_ENABLED = true;
+	public final static String FORCE_API_CONNECTION_TIMEOUT = "120000";
+	public final static String FORCE_API_READ_TIMEOUT = "120000";
 
 	// Place order api properties
-	public final static String PLACEORDER_API_USERNAME = _holder.getProperty(PROPERTY_TYPE.BUILD, "placeorder.api.username");
-	public final static String PLACEORDER_API_PASSWORD = _holder.getProperty(PROPERTY_TYPE.BUILD, "placeorder.api.password");
-	public final static String PlACEORDER_LOGIN_URL = _holder.getProperty(PROPERTY_TYPE.BUILD, "placeorder.login.url");
-	public final static String PLACEORDER_CLIENT_ID = _holder.getProperty(PROPERTY_TYPE.BUILD, "placeorder.client.id");
-	public final static String PLACEORDER_CLIENT_SECRET = _holder.getProperty(PROPERTY_TYPE.BUILD, "placeorder.client.secret");
+	public final static String PLACEORDER_API_USERNAME = "p2cautomationuser@salesforce.com.finupgqa1";
+	public final static String PLACEORDER_API_PASSWORD = "Store123";
+	public final static String PlACEORDER_LOGIN_URL = "https://test.salesforce.com";
+	public final static String PLACEORDER_CLIENT_ID = "3MVG92DxHaawSd0nYhe.zSt6gWPZALrtm7bKQKo09MNe0Kc0bfxJLXCrpeEVX5V4MGIPJWok4a_mS0xhmTX08";
+	public final static String PLACEORDER_CLIENT_SECRET = "9BEF9C3D5964ADE453B5B41B8F98795F7C995023053A1B2B45411788135A1441";
 
 	// Default test product and pricebookentry ids
-	public final static String DEFAULT_PRICEBOOKENTRY_ID = _holder.getProperty(PROPERTY_TYPE.BUILD, "test.pricebookentry.id");
-	public final static String DEFAULT_PRODUCT_ID = _holder.getProperty(PROPERTY_TYPE.BUILD, "test.product.id");
+	public final static String DEFAULT_PRICEBOOKENTRY_ID = "01u0u000000WDllAAG";
+	public final static String DEFAULT_PRODUCT_ID = "01s0u00000071o6AAA";
 
 }
