7908778d96bb9b4b1ade5f7fbb55ea102f6003cf
Krishnan Ganesan <kganesan@salesforce.com>
2019-11-10 02:08:19 -0800

    Update to master

--- src/main/java/com/salesforce/placeorder/quartz/scheduler/PlaceOrderScheduler.java
+++ src/main/java/com/salesforce/placeorder/quartz/scheduler/PlaceOrderScheduler.java
@@ -4,16 +4,24 @@ import static org.quartz.JobBuilder.newJob;
 import static org.quartz.SimpleScheduleBuilder.repeatSecondlyForever;
 import static org.quartz.TriggerBuilder.newTrigger;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.quartz.Job;
 import org.quartz.JobDetail;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
 import org.quartz.Scheduler;
 import org.quartz.Trigger;
 import org.quartz.impl.StdSchedulerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
-import com.salesforce.placeorder.quartz.jobs.LoadContractAndOrdersJob;
+import com.rabbitmq.client.MessageProperties;
+
 
 public class PlaceOrderScheduler {
 	final static Logger logger = LoggerFactory.getLogger(PlaceOrderScheduler.class);
@@ -39,4 +47,31 @@ public class PlaceOrderScheduler {
 			logger.error(e.getMessage(),e);
 		}
 	}
+	
+	public static class LoadContractAndOrdersJob implements Job {
+
+		@Override
+		public void execute(JobExecutionContext context) throws JobExecutionException {
+			// TODO Auto-generated method stub
+			try {
+	            Connection connection = factory.newConnection();
+	            Channel channel = connection.createChannel();
+	            String queueName = "work-queue-1";
+	            Map<String, Object> params = new HashMap<String, Object>();
+	            params.put("x-ha-policy", "all");
+	            channel.queueDeclare(queueName, true, false, false, params);
+
+	            String msg = "LoadContractAndOrdersJob";
+	            byte[] body = msg.getBytes("UTF-8");
+	            channel.basicPublish("", queueName, MessageProperties.PERSISTENT_TEXT_PLAIN, body);
+	            logger.info("Message Sent: " + msg);
+	            connection.close();
+	        }
+	        catch (Exception e) {
+	            logger.error(e.getMessage(), e);
+	        }
+		}
+
+	}
+
 }
