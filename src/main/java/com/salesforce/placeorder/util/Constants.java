58ab00b396161c61cb2809b545e2f31dbdadd58a
Krishnan Ganesan <kganesan@salesforce.com>
2019-11-10 02:24:54 -0800

    Update to master

--- src/main/java/com/salesforce/placeorder/util/Constants.java
+++ src/main/java/com/salesforce/placeorder/util/Constants.java
@@ -1,45 +1,30 @@
 package com.salesforce.placeorder.util;
 
+import com.salesforce.placeorder.util.PropertiesReader.PROPERTY_TYPE;
+
 public class Constants {
+	private final static PropertiesReader _holder = PropertiesReader.getInstance();
+	public static final String ADMIN_USERNAME = _holder.getProperty(PROPERTY_TYPE.BUILD, "admin.username");
+	public static final String ADMIN_PASSWORD = _holder.getProperty(PROPERTY_TYPE.BUILD, "admin.password");
 
-	public static final String ADMIN_USERNAME = System
-			.getProperty("admin.username");
-	public static final String ADMIN_PASSWORD = System
-			.getProperty("admin.password");
-
-	public final static String FORCE_API_USERNAME = System
-			.getProperty("forceapi.username");
-	public final static String FORCE_API_PASSWORD = System
-			.getProperty("forceapi.password");
-	public final static String FORCE_API_ENDPOINT_PARTNER = System
-			.getProperty("forceapi.partner.endpoint");
-	public final static String FORCE_API_ENDPOINT_METADATA = System
-			.getProperty("forceapi.metadata.endpoint");
-	public final static String FORCE_API_ENDPOINT_APEX = System
-			.getProperty("forceapi.apex.endpoint");
-	public final static boolean API_TRACE_ENABLED = (System
-			.getProperty("forceapi.traceenabled") == "true") ? true : false;
-	public final static String FORCE_API_CONNECTION_TIMEOUT = System
-			.getProperty("forceapi.connection.timeout");
-	public final static String FORCE_API_READ_TIMEOUT = System
-			.getProperty("forceapi.read.timeout");
+	public final static String FORCE_API_USERNAME = _holder.getProperty(PROPERTY_TYPE.BUILD, "forceapi.username");
+	public final static String FORCE_API_PASSWORD = _holder.getProperty(PROPERTY_TYPE.BUILD, "forceapi.password");
+	public final static String FORCE_API_ENDPOINT_PARTNER = _holder.getProperty(PROPERTY_TYPE.BUILD, "forceapi.partner.endpoint");
+	public final static String FORCE_API_ENDPOINT_METADATA = _holder.getProperty(PROPERTY_TYPE.BUILD, "forceapi.metadata.endpoint");
+	public final static String FORCE_API_ENDPOINT_APEX = _holder.getProperty(PROPERTY_TYPE.BUILD, "forceapi.apex.endpoint");
+	public final static boolean API_TRACE_ENABLED = (_holder.getProperty(PROPERTY_TYPE.BUILD, "forceapi.traceenabled") == "true") ? true : false;
+	public final static String FORCE_API_CONNECTION_TIMEOUT = _holder.getProperty(PROPERTY_TYPE.BUILD, "forceapi.connection.timeout");
+	public final static String FORCE_API_READ_TIMEOUT = _holder.getProperty(PROPERTY_TYPE.BUILD, "forceapi.read.timeout");
 
 	// Place order api properties
-	public final static String PLACEORDER_API_USERNAME = System
-			.getProperty("placeorder.api.username");
-	public final static String PLACEORDER_API_PASSWORD = System
-			.getProperty("placeorder.api.password");
-	public final static String PlACEORDER_LOGIN_URL = System
-			.getProperty("placeorder.login.url");
-	public final static String PLACEORDER_CLIENT_ID = System
-			.getProperty("placeorder.client.id");
-	public final static String PLACEORDER_CLIENT_SECRET = System
-			.getProperty("placeorder.client.secret");
+	public final static String PLACEORDER_API_USERNAME = _holder.getProperty(PROPERTY_TYPE.BUILD, "placeorder.api.username");
+	public final static String PLACEORDER_API_PASSWORD = _holder.getProperty(PROPERTY_TYPE.BUILD, "placeorder.api.password");
+	public final static String PlACEORDER_LOGIN_URL = _holder.getProperty(PROPERTY_TYPE.BUILD, "placeorder.login.url");
+	public final static String PLACEORDER_CLIENT_ID = _holder.getProperty(PROPERTY_TYPE.BUILD, "placeorder.client.id");
+	public final static String PLACEORDER_CLIENT_SECRET = _holder.getProperty(PROPERTY_TYPE.BUILD, "placeorder.client.secret");
 
 	// Default test product and pricebookentry ids
-	public final static String DEFAULT_PRICEBOOKENTRY_ID = System
-			.getProperty("test.pricebookentry.id");
-	public final static String DEFAULT_PRODUCT_ID = System
-			.getProperty("test.product.id");
+	public final static String DEFAULT_PRICEBOOKENTRY_ID = _holder.getProperty(PROPERTY_TYPE.BUILD, "test.pricebookentry.id");
+	public final static String DEFAULT_PRODUCT_ID = _holder.getProperty(PROPERTY_TYPE.BUILD, "test.product.id");
 
 }
---
+++ src/main/java/com/salesforce/placeorder/util/PropertiesReader.java
@@ -0,0 +1,72 @@
+package com.salesforce.placeorder.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.Set;
+
+public class PropertiesReader {
+
+	public static enum PROPERTY_TYPE {
+		BUILD
+	}
+
+	private final Properties buildProp = new Properties();
+
+	private PropertiesReader() {
+
+		InputStream in2 = this.getClass().getClassLoader()
+				.getResourceAsStream("test.properties");
+
+		System.out
+				.println("PropertiesReader: Read build/mapping properties from file");
+
+		try {
+			buildProp.load(in2);
+			// Load override if available as system properties
+			for (Object key : this.buildProp.keySet()) {
+				String override = System.getProperty((String) key);
+				if (override != null)
+					buildProp.put(key, override);
+
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static class LazyHolder {
+		private static final PropertiesReader INSTANCE = new PropertiesReader();
+	}
+
+	public static PropertiesReader getInstance() {
+		return LazyHolder.INSTANCE;
+	}
+
+	public String getProperty(PROPERTY_TYPE propType, String key) {
+		switch (propType) {
+		case BUILD:
+			return buildProp.getProperty(key) != null ? buildProp.getProperty(
+					key).trim() : buildProp.getProperty(key);
+		}
+		return "";
+	}
+
+	public Set<String> getAllPropertyNames(PROPERTY_TYPE propType) {
+
+		switch (propType) {
+		case BUILD:
+			return buildProp.stringPropertyNames();
+		}
+		return null;
+	}
+
+	public boolean containsKey(PROPERTY_TYPE propType, String key) {
+		switch (propType) {
+		case BUILD:
+			return buildProp.containsKey(key);
+		}
+		return false;
+	}
+
+}
