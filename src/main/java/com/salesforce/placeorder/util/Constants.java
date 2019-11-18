fd3b7a1993cb010e54d129f03a895e4ba44f3975
Krishnan Ganesan <kganesan@salesforce.com>
2019-11-17 19:17:11 -0800

    Adding application.properties

--- src/main/java/com/salesforce/placeorder/util/Constants.java
+++ src/main/java/com/salesforce/placeorder/util/Constants.java
@@ -1,45 +1,34 @@
 package com.salesforce.placeorder.util;
 
+import java.util.Properties;
+
 public class Constants {
+	private static Properties _propertiesHolder = new PropertiesReader("application.properties").get_properties();
+	public static final String ADMIN_USERNAME = _propertiesHolder.getProperty("admin.username");
+	public static final String ADMIN_PASSWORD = _propertiesHolder.getProperty("admin.password");
 
-	public static final String ADMIN_USERNAME = System
-			.getProperty("admin.username");
-	public static final String ADMIN_PASSWORD = System
-			.getProperty("admin.password");
-
-	public final static String FORCE_API_USERNAME = System
-			.getProperty("forceapi.username");
-	public final static String FORCE_API_PASSWORD = System
-			.getProperty("forceapi.password");
-	public final static String FORCE_API_ENDPOINT_PARTNER = System
-			.getProperty("forceapi.partner.endpoint");
-	public final static String FORCE_API_ENDPOINT_METADATA = System
+	public final static String FORCE_API_USERNAME = _propertiesHolder.getProperty("forceapi.username");
+	public final static String FORCE_API_PASSWORD = _propertiesHolder.getProperty("forceapi.password");
+	public final static String FORCE_API_ENDPOINT_PARTNER = _propertiesHolder.getProperty("forceapi.partner.endpoint");
+	public final static String FORCE_API_ENDPOINT_METADATA = _propertiesHolder
 			.getProperty("forceapi.metadata.endpoint");
-	public final static String FORCE_API_ENDPOINT_APEX = System
-			.getProperty("forceapi.apex.endpoint");
-	public final static boolean API_TRACE_ENABLED = (System
-			.getProperty("forceapi.traceenabled") == "true") ? true : false;
-	public final static String FORCE_API_CONNECTION_TIMEOUT = System
+	public final static String FORCE_API_ENDPOINT_APEX = _propertiesHolder.getProperty("forceapi.apex.endpoint");
+	public final static boolean API_TRACE_ENABLED = (_propertiesHolder.getProperty("forceapi.traceenabled") == "true")
+			? true
+			: false;
+	public final static String FORCE_API_CONNECTION_TIMEOUT = _propertiesHolder
 			.getProperty("forceapi.connection.timeout");
-	public final static String FORCE_API_READ_TIMEOUT = System
-			.getProperty("forceapi.read.timeout");
+	public final static String FORCE_API_READ_TIMEOUT = _propertiesHolder.getProperty("forceapi.read.timeout");
 
 	// Place order api properties
-	public final static String PLACEORDER_API_USERNAME = System
-			.getProperty("placeorder.api.username");
-	public final static String PLACEORDER_API_PASSWORD = System
-			.getProperty("placeorder.api.password");
-	public final static String PlACEORDER_LOGIN_URL = System
-			.getProperty("placeorder.login.url");
-	public final static String PLACEORDER_CLIENT_ID = System
-			.getProperty("placeorder.client.id");
-	public final static String PLACEORDER_CLIENT_SECRET = System
-			.getProperty("placeorder.client.secret");
+	public final static String PLACEORDER_API_USERNAME = _propertiesHolder.getProperty("placeorder.api.username");
+	public final static String PLACEORDER_API_PASSWORD = _propertiesHolder.getProperty("placeorder.api.password");
+	public final static String PlACEORDER_LOGIN_URL = _propertiesHolder.getProperty("placeorder.login.url");
+	public final static String PLACEORDER_CLIENT_ID = _propertiesHolder.getProperty("placeorder.client.id");
+	public final static String PLACEORDER_CLIENT_SECRET = _propertiesHolder.getProperty("placeorder.client.secret");
 
 	// Default test product and pricebookentry ids
-	public final static String DEFAULT_PRICEBOOKENTRY_ID = System
-			.getProperty("test.pricebookentry.id");
-	public final static String DEFAULT_PRODUCT_ID = System
-			.getProperty("test.product.id");
+	public final static String DEFAULT_PRICEBOOKENTRY_ID = _propertiesHolder.getProperty("test.pricebookentry.id");
+	public final static String DEFAULT_PRODUCT_ID = _propertiesHolder.getProperty("test.product.id");
 
 }
