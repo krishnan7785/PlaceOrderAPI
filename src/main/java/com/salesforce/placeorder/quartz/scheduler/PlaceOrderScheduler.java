625bac4af0bfbc00eb37202f2fa2ce59f68d5e25
Krishnan Ganesan <kganesan@salesforce.com>
2019-12-15 02:58:59 -0800

    Spring

--- src/main/java/com/salesforce/placeorder/quartz/scheduler/PlaceOrderScheduler.java
+++ src/main/java/com/salesforce/placeorder/quartz/scheduler/PlaceOrderScheduler.java
@@ -11,58 +11,24 @@ import org.quartz.JobExecutionException;
 import org.quartz.Scheduler;
 import org.quartz.Trigger;
 import org.quartz.impl.StdSchedulerFactory;
-<<<<<<< HEAD
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-=======
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.MessageProperties;
->>>>>>> develop
+import com.salesforce.placeorder.quartz.scheduler.PlaceOrderScheduler.LoadContractAndOrdersJob.LoadOrderProductsExistingOrderJob;
 
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 public class PlaceOrderScheduler {
-<<<<<<< HEAD
+
 	final static Logger logger = LogManager.getLogger(PlaceOrderScheduler.class);
 	private static ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-	public static void main(String args[]) {
-		try {
-	        Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
-
-	        scheduler.start();
-
-	        
-	        JobDetail jobDetail1 = newJob(LoadContractAndOrdersJob.class).build();
-	        JobDetail jobDetail2 = newJob(LoadOrderProductsExistingOrderJob.class).build();
-	        Trigger trigger1 = newTrigger()
-	                .startNow()
-	                .withSchedule(repeatSecondlyForever(5))
-	                .build();
-	        Trigger trigger2 = newTrigger()
-	                .startNow()
-	                .withSchedule(repeatSecondlyForever(5))
-	                .build();
-
-	        scheduler.scheduleJob(jobDetail1, trigger1);
-	        scheduler.scheduleJob(jobDetail2, trigger2);
-	        
-		} 
-		catch (Exception e) {
-=======
-
-	final static ConnectionFactory factory = new ConnectionFactory();
 
 	public static void main(String args[]) {
 		try {
-			factory.setUri(System.getenv("CLOUDAMQP_URL"));
 			Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
 
 			scheduler.start();
@@ -76,7 +42,6 @@ public class PlaceOrderScheduler {
 			scheduler.scheduleJob(jobDetail2, trigger2);
 
 		} catch (Exception e) {
->>>>>>> develop
 			// TODO Auto-generated catch block
 			log.error(e.getMessage(), e);
 		} finally {
@@ -108,84 +73,43 @@ public class PlaceOrderScheduler {
 		public void execute(JobExecutionContext context) throws JobExecutionException {
 			// TODO Auto-generated method stub
 			try {
-<<<<<<< HEAD
 				String msg = "LoadContractAndOrdersJob";
-		        RabbitTemplate rabbitTemplate = ctx.getBean(RabbitTemplate.class);
-		        while(true){
-		            String sentimestamp = "Spring Sent at:" + System.currentTimeMillis();
-		            logger.debug(sentimestamp);
-		            logger.debug("\n------------------------------------------------------");
-		            logger.debug(msg);
-		            logger.debug("\n------------------------------------------------------");
-		            byte[] body = msg.getBytes("UTF-8");
-		            rabbitTemplate.send(new Message(body, new MessageProperties()));
-		        }
-	        }
-	        catch (Exception e) {
-	            logger.error(e.getMessage(), e);
-	        }
-=======
-				Connection connection = factory.newConnection();
-				Channel channel = connection.createChannel();
-				String queueName = "work-queue-1";
-				Map<String, Object> params = new HashMap<String, Object>();
-				params.put("x-ha-policy", "all");
-				channel.queueDeclare(queueName, true, false, false, params);
-
-				String msg = "LoadContractAndOrdersJob";
-				byte[] body = msg.getBytes("UTF-8");
-				channel.basicPublish("", queueName, MessageProperties.PERSISTENT_TEXT_PLAIN, body);
-				log.info("Message Sent: " + msg);
-				connection.close();
+				RabbitTemplate rabbitTemplate = ctx.getBean(RabbitTemplate.class);
+				while (true) {
+					String sentimestamp = "Spring Sent at:" + System.currentTimeMillis();
+					logger.debug(sentimestamp);
+					logger.debug("\n------------------------------------------------------");
+					logger.debug(msg);
+					logger.debug("\n------------------------------------------------------");
+					byte[] body = msg.getBytes("UTF-8");
+					rabbitTemplate.send(new Message(body, new MessageProperties()));
+				}
 			} catch (Exception e) {
-				log.error(e.getMessage(), e);
+				logger.error(e.getMessage(), e);
 			}
->>>>>>> develop
 		}
 
-	}
+		public static class LoadOrderProductsExistingOrderJob implements Job {
 
-	public static class LoadOrderProductsExistingOrderJob implements Job {
-
-		@Override
-		public void execute(JobExecutionContext context) throws JobExecutionException {
-			// TODO Auto-generated method stub
-			try {
-<<<<<<< HEAD
-	            String msg = "LoadOrderProductsExistingOrderJob";
-	            RabbitTemplate rabbitTemplate = ctx.getBean(RabbitTemplate.class);
-		        while(true){
-		            String sentimestamp = "Spring Sent at:" + System.currentTimeMillis();
-		            logger.debug(sentimestamp);
-		            logger.debug("\n------------------------------------------------------");
-		            logger.debug(msg);
-		            logger.debug("\n------------------------------------------------------");
-		            byte[] body = msg.getBytes("UTF-8");
-		            rabbitTemplate.send(new Message(body, new MessageProperties()));
-		        }
-	        }
-	        catch (Exception e) {
-	            logger.error(e.getMessage(), e);
-	        }
-=======
-				Connection connection = factory.newConnection();
-				Channel channel = connection.createChannel();
-				String queueName = "work-queue-1";
-				Map<String, Object> params = new HashMap<String, Object>();
-				params.put("x-ha-policy", "all");
-				channel.queueDeclare(queueName, true, false, false, params);
-
-				String msg = "LoadOrderProductsExistingOrderJob";
-				byte[] body = msg.getBytes("UTF-8");
-				channel.basicPublish("", queueName, MessageProperties.PERSISTENT_TEXT_PLAIN, body);
-				log.info("Message Sent: " + msg);
-				connection.close();
-			} catch (Exception e) {
-				log.error(e.getMessage(), e);
+			@Override
+			public void execute(JobExecutionContext context) throws JobExecutionException {
+				// TODO Auto-generated method stub
+				try {
+					String msg = "LoadOrderProductsExistingOrderJob";
+					RabbitTemplate rabbitTemplate = ctx.getBean(RabbitTemplate.class);
+					while (true) {
+						String sentimestamp = "Spring Sent at:" + System.currentTimeMillis();
+						logger.debug(sentimestamp);
+						logger.debug("\n------------------------------------------------------");
+						logger.debug(msg);
+						logger.debug("\n------------------------------------------------------");
+						byte[] body = msg.getBytes("UTF-8");
+						rabbitTemplate.send(new Message(body, new MessageProperties()));
+					}
+				} catch (Exception e) {
+					logger.error(e.getMessage(), e);
+				}
 			}
->>>>>>> develop
 		}
-
 	}
-
 }
--- src/main/java/com/salesforce/placeorder/rabbitmq/worker/LoadContractsAndOrdersWorker.java
+++ src/main/java/com/salesforce/placeorder/rabbitmq/worker/LoadContractsAndOrdersWorker.java
@@ -1,25 +1,11 @@
 package com.salesforce.placeorder.rabbitmq.worker;
 
 import java.util.UUID;
-
-<<<<<<< HEAD
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import com.salesforce.placeorder.builder.AccountVOBuilder;
-import com.salesforce.placeorder.builder.ContractVOBuilder;
-import com.salesforce.placeorder.builder.OpportunityVOBuilder;
-import com.salesforce.placeorder.builder.OrderVOBuilder;
-import com.salesforce.placeorder.builder.UserVOBuilder;
-=======
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.QueueingConsumer;
->>>>>>> develop
 import com.salesforce.placeorder.helper.APIHelper;
 import com.salesforce.placeorder.util.Constants;
 import com.salesforce.placeorder.util.ObjectUtil;
@@ -33,48 +19,22 @@ import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 public class LoadContractsAndOrdersWorker {
-<<<<<<< HEAD
 	final static Logger logger = LogManager.getLogger(LoadContractsAndOrdersWorker.class);
-    static ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-	public static void main(String args[]) {
-		try {
-	        RabbitTemplate rabbitTemplate = ctx.getBean(RabbitTemplate.class);
-	        while (true) {
-	        	logger.debug(" In LoadContractsAndOrdersWorker Checking for message...");
-	        	Message response = rabbitTemplate.receive();
-	            if (response != null) {
-	            	String message = new String(response.getBody(), "UTF-8");
-	            	logger.debug("Spring Recieved:->" + message);
-	            	doWork(message);
-	            	logger.debug("Spring Done with Work:->" + message);
-	            }
-	        }
-=======
+	static ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
 
 	public static void main(String args[]) {
 		try {
-			ConnectionFactory factory = new ConnectionFactory();
-			factory.setUri(System.getenv("CLOUDAMQP_URL"));
-			Connection connection = factory.newConnection();
-			Channel channel = connection.createChannel();
-			String queueName = "work-queue-1";
-			Map<String, Object> params = new HashMap<String, Object>();
-			params.put("x-ha-policy", "all");
-			channel.queueDeclare(queueName, true, false, false, params);
-			QueueingConsumer consumer = new QueueingConsumer(channel);
-			channel.basicConsume(queueName, false, consumer);
-
+			RabbitTemplate rabbitTemplate = ctx.getBean(RabbitTemplate.class);
 			while (true) {
-				QueueingConsumer.Delivery delivery = consumer.nextDelivery();
-				if (delivery != null) {
-					String msg = new String(delivery.getBody(), "UTF-8");
-					log.info("Message Received: " + msg);
-					doWork(msg);
-					log.info("Done  Work: " + msg);
-					channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+				logger.debug(" In LoadContractsAndOrdersWorker Checking for message...");
+				Message response = rabbitTemplate.receive();
+				if (response != null) {
+					String message = new String(response.getBody(), "UTF-8");
+					logger.debug("Spring Recieved:->" + message);
+					doWork(message);
+					logger.debug("Spring Done with Work:->" + message);
 				}
 			}
->>>>>>> develop
 		} catch (Exception e) {
 			log.debug(e.getMessage());
 		}
