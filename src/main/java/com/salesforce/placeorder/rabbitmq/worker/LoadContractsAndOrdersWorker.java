a91015ba80d8addaea134374ba6e96a5f308bd20
Krishnan Ganesan <kganesan@salesforce.com>
2019-12-14 21:35:25 -0800

    lombok

--- src/main/java/com/salesforce/placeorder/rabbitmq/worker/LoadContractsAndOrdersWorker.java
+++ src/main/java/com/salesforce/placeorder/rabbitmq/worker/LoadContractsAndOrdersWorker.java
@@ -54,8 +54,7 @@ public class LoadContractsAndOrdersWorker {
 		if (message != null && message.equals("LoadContractAndOrdersJob")) {
 			try {
 				String random = "PODEMO-" + UUID.randomUUID().toString();
-				AccountVO acc = AccountVO.builder()
-						.name(random).recordType("Sales").numEmployees("2000")
+				AccountVO acc = AccountVO.builder().name(random).recordType("Sales").numEmployees("2000")
 						.billingCity("San francisco").billingCountry("US").billingState("CA")
 						.billingStreet("1 Market Street").billingZip("94105").shippingCity("San Francisco")
 						.shippingCountry("US").shippingStreet("1 Market Street").shippingState("CA")
@@ -64,26 +63,23 @@ public class LoadContractsAndOrdersWorker {
 						.recordType("New Business / Add-On").markForOppurtunityTeamCreate(true)
 						.forcastCategory("Omitted").name(random).stage("01 - Identifying an Opportunity")
 						.closeDate("4/16/2020").build();
-				ContractVO cntr = ContractVO.builder().accountName(random).recordType("Sales")
-						.contractType("New").contractStatus("Draft").autoRenewal("No")
-						.currencyISOCode("USD").billingCity("San francisco")
+				ContractVO cntr = ContractVO.builder().accountName(random).recordType("Sales").contractType("New")
+						.contractStatus("Draft").autoRenewal("No").currencyISOCode("USD").billingCity("San francisco")
 						.billingCompanyName("PlaceOrderTest1").billingCountry("US")
-						.billingEmail("kganesan@salesforce.com").billingFirstName("Krishnan")
-						.billingFrequency("1").billingLanguage("English").billingLastName("Ganesan")
-						.billingState("CA").billingStreet("1 market Street").billingZip("94105")
-						.billingLanguageCode("en_US").contractStartDate("7/1/2019").contractTerm("36")
-						.orderPrebillDays("5").paymentTerms("Net 45").paymentType("Check")
-						.priceBook("CPQ - Direct - Commercial - Ohana - WW - USD").recordType("Sales")
-						.shippingCity("San Francisco").shippingCountry("US")
+						.billingEmail("kganesan@salesforce.com").billingFirstName("Krishnan").billingFrequency("1")
+						.billingLanguage("English").billingLastName("Ganesan").billingState("CA")
+						.billingStreet("1 market Street").billingZip("94105").billingLanguageCode("en_US")
+						.contractStartDate("7/1/2019").contractTerm("36").orderPrebillDays("5").paymentTerms("Net 45")
+						.paymentType("Check").priceBook("CPQ - Direct - Commercial - Ohana - WW - USD")
+						.recordType("Sales").shippingCity("San Francisco").shippingCountry("US")
 						.shippingStreet("1 Market Street").shippingState("CA").shippingZip("94105").build();
 				OrderVO order = OrderVO.builder().opportunityName(random).markForOrderTeamCreate(true)
-						.shippingCity("San Francisco").shippingCountry("US")
-						.shippingStreet("1 Market Street").shippingState("CA").shippingZip("94105")
-						.orderStatus("Draft").orderType("Initial").customerPoRequired("N/A")
-						.orderSubtype("New").orderStartDate("7/1/2019").orderTerm("12")
+						.shippingCity("San Francisco").shippingCountry("US").shippingStreet("1 Market Street")
+						.shippingState("CA").shippingZip("94105").orderStatus("Draft").orderType("Initial")
+						.customerPoRequired("N/A").orderSubtype("New").orderStartDate("7/1/2019").orderTerm("12")
 						.poNumber("12345").poAmount("5000").evaluateRamp(false).build();
-				UserVO user = UserVO.builder().username(Constants.ADMIN_USERNAME)
-						.password(Constants.ADMIN_PASSWORD).build();
+				UserVO user = UserVO.builder().username(Constants.ADMIN_USERNAME).password(Constants.ADMIN_PASSWORD)
+						.build();
 				String pricebookentryid = Constants.DEFAULT_PRICEBOOKENTRY_ID;
 				String pricebook2id = Constants.DEFAULT_PRODUCT_ID;
 				String accountid = ObjectUtil.createTestAccount(user, acc);
@@ -98,6 +94,26 @@ public class LoadContractsAndOrdersWorker {
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				log.error(e.getMessage());
+			} finally {
+				Runtime.getRuntime().addShutdownHook(new Thread() {
+					@Override
+					public void run() {
+						final java.lang.management.ThreadMXBean threadMXBean = java.lang.management.ManagementFactory
+								.getThreadMXBean();
+						final java.lang.management.ThreadInfo[] threadInfos = threadMXBean
+								.getThreadInfo(threadMXBean.getAllThreadIds(), 100);
+						for (java.lang.management.ThreadInfo threadInfo : threadInfos) {
+							log.debug(threadInfo.getThreadName());
+							final Thread.State state = threadInfo.getThreadState();
+							log.debug("   java.lang.Thread.State: " + state);
+							final StackTraceElement[] stackTraceElements = threadInfo.getStackTrace();
+							for (final StackTraceElement stackTraceElement : stackTraceElements) {
+								log.debug("        at " + stackTraceElement);
+							}
+							log.debug("\n");
+						}
+					}
+				});
 			}
 		}
 	}
--- src/main/java/com/salesforce/placeorder/rabbitmq/worker/LoadOrderProductsExistingOrderWorker.java
+++ src/main/java/com/salesforce/placeorder/rabbitmq/worker/LoadOrderProductsExistingOrderWorker.java
@@ -62,26 +62,23 @@ public class LoadOrderProductsExistingOrderWorker {
 						.recordType("New Business / Add-On").markForOppurtunityTeamCreate(true)
 						.forcastCategory("Omitted").name(random).stage("01 - Identifying an Opportunity")
 						.closeDate("4/16/2020").build();
-				ContractVO cntr = ContractVO.builder().accountName(random).recordType("Sales")
-						.contractType("New").contractStatus("Draft").autoRenewal("No")
-						.currencyISOCode("USD").billingCity("San francisco")
+				ContractVO cntr = ContractVO.builder().accountName(random).recordType("Sales").contractType("New")
+						.contractStatus("Draft").autoRenewal("No").currencyISOCode("USD").billingCity("San francisco")
 						.billingCompanyName("PlaceOrderTest1").billingCountry("US")
-						.billingEmail("kganesan@salesforce.com").billingFirstName("Krishnan")
-						.billingFrequency("1").billingLanguage("English").billingLastName("Ganesan")
-						.billingState("CA").billingStreet("1 market Street").billingZip("94105")
-						.billingLanguageCode("en_US").contractStartDate("7/1/2020").contractTerm("36")
-						.orderPrebillDays("5").paymentTerms("Net 45").paymentType("Check")
-						.priceBook("CPQ - Direct - Commercial - Ohana - WW - USD").recordType("Sales")
-						.shippingCity("San Francisco").shippingCountry("US")
+						.billingEmail("kganesan@salesforce.com").billingFirstName("Krishnan").billingFrequency("1")
+						.billingLanguage("English").billingLastName("Ganesan").billingState("CA")
+						.billingStreet("1 market Street").billingZip("94105").billingLanguageCode("en_US")
+						.contractStartDate("7/1/2020").contractTerm("36").orderPrebillDays("5").paymentTerms("Net 45")
+						.paymentType("Check").priceBook("CPQ - Direct - Commercial - Ohana - WW - USD")
+						.recordType("Sales").shippingCity("San Francisco").shippingCountry("US")
 						.shippingStreet("1 Market Street").shippingState("CA").shippingZip("94105").build();
 				OrderVO order = OrderVO.builder().opportunityName(random).markForOrderTeamCreate(true)
-						.shippingCity("San Francisco").shippingCountry("US")
-						.shippingStreet("1 Market Street").shippingState("CA").shippingZip("94105")
-						.orderStatus("Draft").orderType("Initial").customerPoRequired("N/A")
-						.orderSubtype("New").orderStartDate("7/1/2020").orderTerm("12")
+						.shippingCity("San Francisco").shippingCountry("US").shippingStreet("1 Market Street")
+						.shippingState("CA").shippingZip("94105").orderStatus("Draft").orderType("Initial")
+						.customerPoRequired("N/A").orderSubtype("New").orderStartDate("7/1/2020").orderTerm("12")
 						.poNumber("12345").poAmount("5000").evaluateRamp(false).build();
-				UserVO user = UserVO.builder().username(Constants.ADMIN_USERNAME)
-						.password(Constants.ADMIN_PASSWORD).build();
+				UserVO user = UserVO.builder().username(Constants.ADMIN_USERNAME).password(Constants.ADMIN_PASSWORD)
+						.build();
 				String pricebookentryid = Constants.DEFAULT_PRICEBOOKENTRY_ID;
 				String pricebook2id = Constants.DEFAULT_PRODUCT_ID;
 				String accountid = ObjectUtil.createTestAccount(user, acc);
@@ -96,6 +93,26 @@ public class LoadOrderProductsExistingOrderWorker {
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				log.error(e.getMessage());
+			} finally {
+				Runtime.getRuntime().addShutdownHook(new Thread() {
+					@Override
+					public void run() {
+						final java.lang.management.ThreadMXBean threadMXBean = java.lang.management.ManagementFactory
+								.getThreadMXBean();
+						final java.lang.management.ThreadInfo[] threadInfos = threadMXBean
+								.getThreadInfo(threadMXBean.getAllThreadIds(), 100);
+						for (java.lang.management.ThreadInfo threadInfo : threadInfos) {
+							log.debug(threadInfo.getThreadName());
+							final Thread.State state = threadInfo.getThreadState();
+							log.debug("   java.lang.Thread.State: " + state);
+							final StackTraceElement[] stackTraceElements = threadInfo.getStackTrace();
+							for (final StackTraceElement stackTraceElement : stackTraceElements) {
+								log.debug("        at " + stackTraceElement);
+							}
+							log.debug("\n");
+						}
+					}
+				});
 			}
 		}
 	}
