5643c7db064df1279c4c5a5eef52303c9099cf5b
Krishnan Ganesan <kganesan@salesforce.com>
2019-12-15 03:34:07 -0800

    Spring

--- src/main/java/com/salesforce/placeorder/util/RabbitMQUtil.java
+++ src/main/java/com/salesforce/placeorder/util/RabbitMQUtil.java
@@ -1,5 +1,6 @@
 package com.salesforce.placeorder.util;
 
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
@@ -9,7 +10,12 @@ import com.rabbitmq.client.ConnectionFactory;
 public class RabbitMQUtil {
 	public static ConnectionFactory getConnectionFactory() throws KeyManagementException, NoSuchAlgorithmException, URISyntaxException  {
         ConnectionFactory factory = new ConnectionFactory();
-        factory.setUri(System.getenv("CLOUDAMQP_URL"));
+        URI uri = new URI(System.getenv("CLOUDAMQP_URL"));
+        factory.setUsername(uri.getUserInfo().split(":")[0]);
+        factory.setPassword(uri.getUserInfo().split(":")[1]);
+        factory.setHost(uri.getHost());
+        factory.setPort(uri.getPort());
+        factory.setVirtualHost(uri.getPath().substring(1));
         return factory;
     }
 }
