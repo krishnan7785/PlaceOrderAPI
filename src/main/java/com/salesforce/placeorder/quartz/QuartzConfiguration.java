b71b278e8cdf18a9a76d59cfac9bbe6bf3e5ecad
Krishnan Ganesan <kganesan@salesforce.com>
2020-01-02 14:30:33 -0800

    Postgres

--- src/main/java/com/salesforce/placeorder/quartz/QuartzConfiguration.java
+++ src/main/java/com/salesforce/placeorder/quartz/QuartzConfiguration.java
@@ -1,134 +1,97 @@
 package com.salesforce.placeorder.quartz;
 
 
-import java.io.IOException;
-import java.util.Calendar;
-import java.util.Properties;
-
+import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.config.PropertiesFactoryBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.quartz.QuartzDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
 import org.springframework.scheduling.quartz.JobDetailFactoryBean;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 import org.springframework.scheduling.quartz.SimpleTriggerFactoryBean;
 import org.springframework.scheduling.quartz.SpringBeanJobFactory;
-import org.apache.commons.lang3.ArrayUtils;
-import org.quartz.CronTrigger;
-import org.quartz.Job;
 import org.quartz.JobDetail;
 import org.quartz.SimpleTrigger;
 import org.quartz.Trigger;
-
 import com.salesforce.placeorder.quartz.AutowiringSpringBeanJobFactory;
 import lombok.extern.log4j.Log4j2;
 @Log4j2
 @Configuration
+@EnableAutoConfiguration
 public class QuartzConfiguration {
-	public static final String CRON_EVERY_FIFTEEN_SECONDS = "0/15 0 0 ? * * *";
-	 
-	private ApplicationContext applicationContext;
-    private DataSource dataSource;
 
-    public QuartzConfiguration(ApplicationContext applicationContext, DataSource dataSource){
-    	this.applicationContext = applicationContext;
-        this.dataSource = dataSource;
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @PostConstruct
+    public void init() {
+        log.debug("Quartz Initialization...");
     }
-	
+
     @Bean
     public SpringBeanJobFactory springBeanJobFactory() {
-    	AutowiringSpringBeanJobFactory jobFactory = new AutowiringSpringBeanJobFactory();
+        AutowiringSpringBeanJobFactory jobFactory = new AutowiringSpringBeanJobFactory();
+        log.debug("Configuring Job factory");
+
         jobFactory.setApplicationContext(applicationContext);
         return jobFactory;
     }
- 
+
     @Bean
-    public SchedulerFactoryBean scheduler(Trigger... triggers)
-            throws IOException {
-        SchedulerFactoryBean factory = new SchedulerFactoryBean();
-        factory.setOverwriteExistingJobs(true);
-        factory.setAutoStartup(true);
-        factory.setDataSource(this.dataSource);
-        factory.setJobFactory(springBeanJobFactory());
-        if (ArrayUtils.isNotEmpty(triggers)) {
-            factory.setTriggers(triggers);
-        }
-        factory.setQuartzProperties(quartzProperties());
-        return factory;
+    public SchedulerFactoryBean scheduler(Trigger trigger, JobDetail job, DataSource quartzDataSource) {
+
+        SchedulerFactoryBean schedulerFactory = new SchedulerFactoryBean();
+        schedulerFactory.setConfigLocation(new ClassPathResource("quartz.properties"));
+
+        log.debug("Setting the Scheduler up");
+        schedulerFactory.setJobFactory(springBeanJobFactory());
+        schedulerFactory.setJobDetails(job);
+        schedulerFactory.setTriggers(trigger);
+
+        // Comment the following line to use the default Quartz job store.
+        schedulerFactory.setDataSource(quartzDataSource);
+
+        return schedulerFactory;
     }
-    
+
     @Bean
-    public Properties quartzProperties() throws IOException {
-        PropertiesFactoryBean propertiesFactoryBean = new PropertiesFactoryBean();
-        propertiesFactoryBean.setLocation(new ClassPathResource("quartz.properties"));
-        Properties properties;
-        try {
-            propertiesFactoryBean.afterPropertiesSet();
-            properties = propertiesFactoryBean.getObject();
-        }
-        catch (IOException e) {
-            throw new RuntimeException("Unable to load quartz.properties", e);
-        }
- 
-        return properties;
-    }
-    
-    static SimpleTriggerFactoryBean createTrigger(JobDetail jobDetail, long pollFrequencyMs, String triggerName) {
-        log.debug("createTrigger(jobDetail={}, pollFrequencyMs={}, triggerName={})", jobDetail.toString(), pollFrequencyMs, triggerName);
+    public JobDetailFactoryBean jobDetail() {
 
-        SimpleTriggerFactoryBean factoryBean = new SimpleTriggerFactoryBean();
-        factoryBean.setJobDetail(jobDetail);
-        factoryBean.setStartDelay(0L);
-        factoryBean.setRepeatInterval(pollFrequencyMs);
-        factoryBean.setName(triggerName);
-        factoryBean.setRepeatCount(SimpleTrigger.REPEAT_INDEFINITELY);
-        factoryBean.setMisfireInstruction(SimpleTrigger.MISFIRE_INSTRUCTION_RESCHEDULE_NEXT_WITH_REMAINING_COUNT);
-
-        return factoryBean;
+        JobDetailFactoryBean jobDetailFactory = new JobDetailFactoryBean();
+        jobDetailFactory.setJobClass(LoadContractAndOrdersJob.class);
+        jobDetailFactory.setName("Qrtz_Job_Detail");
+        jobDetailFactory.setDescription("Invoke Sample Job service...");
+        jobDetailFactory.setDurability(true);
+        return jobDetailFactory;
     }
 
-    static CronTriggerFactoryBean createCronTrigger(JobDetail jobDetail, String cronExpression, String triggerName) {
-        log.debug("createCronTrigger(jobDetail={}, cronExpression={}, triggerName={})", jobDetail.toString(), cronExpression, triggerName);
+    @Bean
+    public SimpleTriggerFactoryBean trigger(JobDetail job) {
 
-        // To fix a known issue with time-based cron jobs
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.SECOND, 0);
-        calendar.set(Calendar.MILLISECOND, 0);
+        SimpleTriggerFactoryBean trigger = new SimpleTriggerFactoryBean();
+        trigger.setJobDetail(job);
 
-        CronTriggerFactoryBean factoryBean = new CronTriggerFactoryBean();
-        factoryBean.setJobDetail(jobDetail);
-        factoryBean.setCronExpression(cronExpression);
-        factoryBean.setStartTime(calendar.getTime());
-        factoryBean.setStartDelay(0L);
-        factoryBean.setName(triggerName);
-        factoryBean.setMisfireInstruction(CronTrigger.MISFIRE_INSTRUCTION_DO_NOTHING);
+        int frequencyInSec = 10;
+        log.debug("Configuring trigger to fire every {} seconds", frequencyInSec);
 
-        return factoryBean;
+        trigger.setRepeatInterval(frequencyInSec * 1000);
+        trigger.setRepeatCount(SimpleTrigger.REPEAT_INDEFINITELY);
+        trigger.setName("Qrtz_Trigger");
+        return trigger;
     }
 
-    static JobDetailFactoryBean createJobDetail(Class<? extends Job> jobClass, String jobName) {
-        log.debug("createJobDetail(jobClass={}, jobName={})", jobClass.getName(), jobName);
-        JobDetailFactoryBean factoryBean = new JobDetailFactoryBean();
-        factoryBean.setName(jobName);
-        factoryBean.setJobClass(jobClass);
-        factoryBean.setDurability(true);
-        return factoryBean;
-    }
-    
-
-    @Bean(name = "loadContractAndOrdersTrigger")
-    public CronTriggerFactoryBean triggerLoadContractAndOrders(@Qualifier("loadContractAndOrders") JobDetail jobDetail) {
-        return QuartzConfiguration.createCronTrigger(jobDetail, CRON_EVERY_FIFTEEN_SECONDS, "loadContractAndOrdersTrigger");
-    }
-    
-    @Bean(name = "loadContractAndOrders")
-    public JobDetailFactoryBean jobLoadContractAndOrders() {
-        return QuartzConfiguration.createJobDetail(LoadContractAndOrdersJob.class, "Load Contract and Orders Job");
+    @Bean
+    @QuartzDataSource
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource datasource() {
+        return DataSourceBuilder.create().build();
     }
 }
